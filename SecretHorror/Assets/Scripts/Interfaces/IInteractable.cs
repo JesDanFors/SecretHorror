12734a68bfa2e6275c045dc79767a6bc6f5bde6a
Jesper Danielsson <jesperdanielsson1@gmail.com>
2023-08-18 20:11:37 +0200

    feat: Add a simple interface and demo cube for interactable system

--- SecretHorror/Assets/Scripts/Interfaces/IInteractable.cs
+++ SecretHorror/Assets/Scripts/Interfaces/IInteractable.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public interface IInteractable 
 {
-   public void Interact(){
-      Debug.Log("I was Interacted");
-      //TODO: Make sure that all object that can be interacted with uses this
+   void Interact(){
    }
 }
--- SecretHorror/Assets/Scripts/PlayerRelated/FirstPersonCamera.cs
+++ SecretHorror/Assets/Scripts/PlayerRelated/FirstPersonCamera.cs
@@ -8,6 +8,8 @@ public class FirstPersonCamera : MonoBehaviour
     float lookSpeed = 2.0f;
     [SerializeField]
     float lookXLimit = 45.0f;
+    [SerializeField] 
+    float interactRange = 4.0f;
     
     Camera playerCamera;
     float rotationX;
@@ -23,9 +25,21 @@ public class FirstPersonCamera : MonoBehaviour
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+            Interact();
     }
 
     void Interact(){
-        //TODO: Look for interactables when raycasting
+        if (Input.GetKeyDown(KeyCode.E)){
+            CastRay();
+        }
+    }
+
+    void CastRay(){
+        RaycastHit hitInfo = new RaycastHit();
+        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, interactRange);
+        if (hit){
+            GameObject hitObject = hitInfo.transform.gameObject;
+            hitObject.GetComponent<IInteractable>()?.Interact();
+        }
     }
 }
